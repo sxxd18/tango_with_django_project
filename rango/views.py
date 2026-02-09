8f5cb07d4ed3c02f0e6b9dc62502235e2abcd24b
Saad Khalid <3007784k@student.gla.ac.uk>
2026-02-09 21:03:16 +0000

    Completed Chapter 8 template inheritance and relative URLs

--- rango/views.py
+++ rango/views.py
@@ -17,7 +17,7 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
